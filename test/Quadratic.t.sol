04e975cbf724dff987b2b1fb2f9e1e3144f5b2b0
g <gurwindersingh00064@gmail.com>
2024-01-22 17:00:28 +0530

    invariant testing

---
+++ test/Quadratic.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Quadratic.sol";
+import "./handlers/Handler_2.sol";
+
+contract InvariantQuadratic is Test {
+    Quadratic quadratic;
+    Handler_2 handler;
+
+    function setUp() external {
+        quadratic = new Quadratic();
+        handler = new Handler_2(quadratic);
+
+        targetContract(address(handler));
+    }
+
+    function invariant_NotOkay() external {
+        assertTrue(quadratic.ok());
+    }
+    function notOkay(int x) external {
+        x = bound(x, 10_000, 100_000);
+        quadratic.notOkay(x);
+    }
+}
